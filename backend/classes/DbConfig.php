edc3f295d735254fe419cecb10981e81412c1db4
Daan Berg <6013014@mborijnland.nl>
2023-04-12 19:29:11 +0200

    Navbar, container + db connectie

---
+++ backend/classes/DbConfig.php
@@ -0,0 +1,17 @@
+<?php
+
+class DbConfig{
+
+    public function connect(){
+        try{
+            $conn = new PDO("mysql:host=localhost;dbname=php_tem", 'root', '');
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    require_once 'partial/header.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-7"><div class="p-3 border bg-light ">Hoi</div> </div>
+            <div class="col-lg-3"><div class="p-3 border bg-light">Hoi</div> </div>
+        </div>
+    
+    </div>
+</body>
+</html>

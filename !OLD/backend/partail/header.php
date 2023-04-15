b54b223e010673f3306537be76ead865f52cf77e
Ruurd <74601593+Lux42069@users.noreply.github.com>
2023-04-15 11:19:10 +0200

    removed and added old exam

---
+++ !OLD/backend/partail/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/header.css?v=<?php echo time(); ?>">
+    <title>Document</title>
+</head>
+<header>
+
+<!--laptop/ipad nav-->
+  <div class="nav">
+    <div class="navitems">
+      <a href="home.php">Home</a>
+    <br><br>
+      
+      <a href="contact.php">Contact</a>
+    <br><br>
+      
+      <a href="webshop.php">Webshop</a>
+    <br><br>
+      
+      <a href="login.php">Inloggen</a>
+    </div>
+  </div>
+
+<!--phone nav-->
+<div class="mobile-container">
+
+<!-- Top Navigation Menu -->
+<div class="topnav">
+  <h1 href="#home" class="active">Je haar zit goed</h1>
+  <div id="myLinks">
+    <a href="home.php">Home</a>
+    <a href="contact.php">Contact</a>
+    <a href="webshop.php">Webshop</a>
+    <a href="login.php">login</a>
+  </div>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+
+<!-- End smartphone / tablet look -->
+</div>
+
+<script>
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+</script>
+
+</header>
+
---
+++ !OLD/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/homestyle.css?v=<?php echo time(); ?>">
+    <title>Document</title>
+</head>
+    <?php
+        require_once './partail/header.php';
+        /*require_once './partail/footer.php'; */
+    ?>
+
+<body>
+    <section class="flex-container">
+        <div>
+            <h1>Je haar zit goed</h1>   
+        </div>
+
+        <div>
+            <h2>Specialisatie</h2>
+        </div>
+        <div>
+        <p>
+            Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. Proin vehicula, odio ut euismod venenatis, 
+            nibh neque consectetur metus, id mollis eros purus vel sapien. Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. Morbi ac posuere mi, nec placerat dolor. Donec mauris dui, rutrum sed consectetur eu, 
+            bibendum non est. Integer sodales leo sed efficitur efficitur. Phasellus vitae risus risus. Aliquam ante ante, imperdiet ut lacus eu, 
+            lacinia mattis risus. Quisque eu dui sed nisl suscipit maximus nec sit amet mauris. Morbi at rhoncus dui, pharetra dignissim ipsum. 
+            Aliquam erat volutpat. Vestibulum nec condimentum sem. Sed ligula ligula, congue et dui a, auctor porta erat.
+            Aenean et hendrerit enim. Nunc egestas vel orci nec laoreet. Aenean in condimentum nunc. Mauris elementum tempus arcu rutrum sodales. 
+            Ut blandit nibh in enim luctus vehicula. In hac habitasse platea dictumst. Nunc dapibus et ipsum eget pretium. Pellentesque vulputate 
+            nibh cursus, blandit ligula eget, ultrices. 
+        </p>
+        </div>
+    </section>
+
+    <section class="flex-container">    
+        <div>
+            <p>
+                <h2>Doel</h2>
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit. Proin vehicula, odio ut euismod venenatis, 
+                    nibh neque consectetur metus, id mollis eros purus vel sapien. Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit. Morbi ac posuere mi, nec placerat dolor. Donec mauris dui, rutrum sed consectetur eu, 
+                    bibendum non est. 
+                    Integer sodales leo sed efficitur efficitur. Phasellus vitae risus risus. Aliquam ante ante, imperdiet ut lacus eu, 
+                    lacinia mattis risus. 
+            </p>
+        
+            <p>
+                <h2>Visie</h2>
+                    Quisque eu dui sed nisl suscipit maximus nec sit amet mauris. Morbi at rhoncus dui, pharetra dignissim ipsum. 
+                    Aliquam erat volutpat. 
+                    Vestibulum nec condimentum sem. Sed ligula ligula, congue et dui a, auctor porta erat.
+                    Aenean et hendrerit enim. Nunc egestas vel orci nec laoreet. Aenean in condimentum nunc. Mauris elementum tempus arcu rutrum sodales. 
+                    Ut blandit nibh in enim luctus vehicula. In hac habitasse platea dictumst. Nunc dapibus et ipsum eget pretium. Pellentesque vulputate 
+                    nibh cursus, blandit ligula eget, ultrices. 
+            </p>
+        </div>
+    </section>
+
+
+</body>
+</html>

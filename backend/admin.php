c641ae4e10c7ecbc676d0e9255ad380ffd2cac6e
Daan Berg <6013014@mborijnland.nl>
2023-04-17 17:53:15 +0200

    linkies werkend

--- backend/admin.php
+++ backend/admin.php
@@ -16,12 +16,12 @@ require_once 'partial/header.php';
     <div class="container">
         <div class="row justify-content-around text-center">
             <div class="col-lg-3 border bg-light "><a href='AllBars.php' class="nav-link">Bekijk alle Barz </a></div>
-            <div class="col-lg-3 border bg-light" >Bekijk alle Users</div>
+            <div class="col-lg-3 border bg-light" ><a href='allProducts.php' class="nav-link">Bekijk alle Producten</a></div>
             <div class="col-lg-3 border bg-light">Bekijk ...</div>
         </div>
         <div class="row justify-content-around text-center mt-3">
             <div class="col-lg-3 border bg-light"><a href="AddBars.php" class="nav-link">Voeg Barz toe </a></div>
-            <div class="col-lg-3 border bg-light" >Voeg Users toe</div>
+            <div class="col-lg-3 border bg-light"><a href="AddProducts.php" class="nav-link"> Voeg Producten toe</a></div>
             <div class="col-lg-3 border bg-light">Voeg ... toe</div>
         </div>
     </div>
